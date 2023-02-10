322c64a9a5fc1ea1e4309dc3e241455125d7f0b1
mukhqmed <123320324+mukhqmed@users.noreply.github.com>
2023-02-10 23:05:48 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,38 @@
+import java.sql.*;
 import java.util.Scanner;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Scanner input = new Scanner(System.in);
-        List<Automobile> automobiles = List.of(
-                /*new Automobile(false, 1998, true),
-                new Automobile(true, 2003, false)*/
-        );
+        JDBC jdbc = new JDBC();
+        List<Automobile> automobiles = jdbc.getAutomobiles();
+        for (Automobile automobile : automobiles) {
+            System.out.println(automobile.getInfo());
+        }
         TransmissionBoxFilter transmission = new TransmissionBoxFilter();
         ReleaseYearFilter releaseYear = new ReleaseYearFilter();
         automobiles = transmission.filter(automobiles);
-        System.out.println(automobiles);
         menu(input, automobiles);
     }
 
-    public static void menu(Scanner scanner, List<Automobile> automobiles) {
+    public static void menu(Scanner scanner, List<Automobile> automobiles) throws SQLException {
         boolean isOpen = true;
         while (isOpen) {
             System.out.println("Choose an option by the number below:");
-            System.out.println("1. Buy a car");
+            System.out.println("1. Find a car");
             System.out.println("2. Publish new car");
             System.out.println("3. Exit");
             switch (scanner.nextInt()) {
-                case 1 -> buyCar(automobiles);
-                case 2 -> publishCar(automobiles);
+                case 1 -> findCar();
+                case 2 -> publishCar();
                 case 3 -> isOpen = false;
                 default -> System.out.println("Pick a number between 1 and 3");
             }
         }
     }
 
-    private static void buyCar(List<Automobile> automobiles) {
+    private static void findCar() {
         Scanner scanner = new Scanner(System.in);
         TransmissionBoxFilter filterTransmission = new TransmissionBoxFilter();
         ConditionFilter filterCondition = new ConditionFilter();
@@ -41,8 +42,7 @@ public class Main {
         ColorFilter filterColor = new ColorFilter();
         BrandFilter filterBrand = new BrandFilter();
         ModelFilter filterModel = new ModelFilter();
-
-        System.out.println("You should follow instructions to buy a car.");
+        System.out.println("You should follow instructions to find a car.");
         inputCondition(scanner, filterCondition);
         inputTransmission(scanner, filterTransmission);
         inputReleaseYearRange(scanner, filterYear);
@@ -51,34 +51,67 @@ public class Main {
         inputColor(scanner, filterColor);
         inputBrand(scanner, filterBrand);
         inputModel(scanner, filterModel);
-        for (Automobile automobile : automobiles) {
-            System.out.println(automobile.toString());
+        JDBC jdbc = new JDBC();
+        List<Automobile> automobiles = jdbc.getAutomobiles();
+        List<Automobile> filteredAutomobiles = filterTransmission.filter(automobiles);
+        filteredAutomobiles = filterCondition.filter(filteredAutomobiles);
+        filteredAutomobiles = filterYear.filter(filteredAutomobiles);
+        filteredAutomobiles = filterPrice.filter(filteredAutomobiles);
+        filteredAutomobiles = filterVolume.filter(filteredAutomobiles);
+        filteredAutomobiles = filterColor.filter(filteredAutomobiles);
+        filteredAutomobiles = filterBrand.filter(filteredAutomobiles);
+        filteredAutomobiles = filterModel.filter(filteredAutomobiles);
+        for (Automobile automobile : filteredAutomobiles) {
+            System.out.println(automobile);
         }
     }
 
 
-    private static void publishCar(List<Automobile> automobiles) {
+
+    private static Connection getConnection() throws SQLException {
+        String url = "jdbc:postgresql://localhost:5432/tuttorial";
+        String username = "postgres";
+        String password = "mukha";
+        return DriverManager.getConnection(url, username, password);
+    }
+
+    private static void publishCar() throws SQLException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("You should follow instructions to publish a car.");
+        System.out.println("Write ID:");
+        int id = scanner.nextInt();
         System.out.println("Choose the condition of the car:\nTrue - new\nFalse - used");
         boolean isNew = scanner.nextBoolean();
-        System.out.println("Choose the transmission:\nTrue - mechanic\nFalse - automatic");
-        boolean transmissionType = scanner.nextBoolean();
+        System.out.println("Choose the transmission:\nTrue - automatic\nFalse - mechanic");
+        boolean isAutomatic = scanner.nextBoolean();
         System.out.println("Enter a year.");
-        int year = scanner.nextInt();
+        int releaseYear = scanner.nextInt();
         System.out.println("Enter a price.");
         int price = scanner.nextInt();
         System.out.println("Enter a color.");
-        String color = scanner.nextLine();
+        String color = scanner.next();
         System.out.println("Enter a volume of engine.");
-        double volume = scanner.nextDouble();
+        double volumeOfEngine = scanner.nextDouble();
         System.out.println("Enter the brand of car.");
-        String brand = scanner.nextLine();
+        String brand = scanner.next();
         System.out.println("Enter the model of car.");
-        String model = scanner.nextLine();
-        Automobile automobile = new Automobile(transmissionType, year, isNew, color, price, volume, brand, model);
-        automobiles.add(automobile);
+        String model = scanner.next();
+        Connection connection = getConnection();
+        String sql = "INSERT INTO auto (id, is_automatic, release_year, is_new, color, price, volume_of_engine, brand, model) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setInt(1, id);
+        statement.setInt(3, releaseYear);
+        statement.setBoolean(4, isNew);
+        statement.setString(5, color);
+        statement.setInt(6, price);
+        statement.setDouble(7, volumeOfEngine);
+        statement.setString(8, brand);
+        statement.setString(9, model);
+        statement.setBoolean(2, isAutomatic);
+        statement.executeUpdate();
     }
+
+
 
 
     private static void inputReleaseYearRange(Scanner scanner, ReleaseYearFilter filter) {
@@ -90,7 +123,7 @@ public class Main {
     }
 
     public static void inputTransmission(Scanner scanner, TransmissionBoxFilter filter) {
-        System.out.println("Choose the transmission:\nTrue - mechanic\nFalse - automatic");
+        System.out.println("Choose the transmission:\nTrue - automatic\nFalse - mechanic");
         filter.setAutomatic(scanner.nextBoolean());
     }
 
@@ -100,8 +133,8 @@ public class Main {
     }
 
     public static void inputColor(Scanner scanner, ColorFilter filter) {
-        System.out.print("Write a color of car by lower case letters.");
-        filter.setColor(scanner.nextLine());
+        System.out.println("Write a color of car by lower case letters.");
+        filter.setColor(scanner.next());
     }
 
     public static void inputPriceRange(Scanner scanner, PriceRangeFilter filter) {
@@ -119,11 +152,11 @@ public class Main {
 
     public static void inputBrand(Scanner scanner, BrandFilter filter) {
         System.out.println("Enter the brand.");
-        filter.setBrand(scanner.nextLine());
+        filter.setBrand(scanner.next());
     }
 
     public static void inputModel(Scanner scanner, ModelFilter filter) {
         System.out.println("Enter the model.");
-        filter.setModel(scanner.nextLine());
+        filter.setModel(scanner.next());
     }
 }
